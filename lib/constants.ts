3964ad08c6694b84be027f000352bb63d957a868
babulalbera8824 <babulalbera8824@gmail.com>
2026-09-11 13:25:53 +0530

    Define site constants for name, currency, and categories
    Added constants for site name, currency, and categories.

---
+++ lib/constants.ts
@@ -0,0 +1,13 @@
+export const SITE_NAME = "QuickKart";
+export const CURRENCY = "₹";
+export const CATEGORIES = [
+  "Fruits & Vegetables",
+  "Dairy & Breakfast",
+  "Atta, Rice & Dal",
+  "Oil & Masala",
+  "Snacks",
+  "Beverages",
+  "Personal Care",
+  "Cleaning Essentials",
+  "Baby Care",
+];
