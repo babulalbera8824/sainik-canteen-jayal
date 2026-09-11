75084257b7a9f36edc09aef03ffe9f0e53e18810
babulalbera8824 <babulalbera8824@gmail.com>
2026-09-11 13:15:02 +0530

    Add additional remote image patterns to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { remotePatterns: [{ protocol: 'https', hostname: '**.supabase.co' }] },
-}
-module.exports = nextConfig
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: '**.supabase.co' },
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: '**.supabase.in' },
+    ],
+  },
+};
+export default nextConfig;
